2c65449d19e375145db2a7d649e5d3764b374064
timetraveler <timetraveler930@gmail.com>
2023-08-08 02:06:28 +0900

    Implement sized_queue class

---
+++ include/nested-shaper/container.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+namespace ns
+{
+    // Parent class of sized_queue
+    template <typename T>
+    class queue
+    {
+    public:
+        using value_type = T;
+        using reference = T &;
+        using const_reference = const T &;
+        using size_type = size_t;
+
+        // Test whether container is empty
+        virtual bool empty() const = 0;
+        // Test whether container is full
+        virtual bool full() const = 0;
+        // Return size
+        virtual size_type size() const = 0;
+        // Return maximum size <= capacity
+        virtual size_type max_size() const = 0;
+        // Return capacity, preallocated buffer by derived template argument
+        virtual size_type capacity() const = 0;
+        // Set as empty
+        virtual void reset() = 0;
+        // Set maximum size of queue
+        virtual void resize(const size_type &size) = 0;
+        // Fill all data as given value
+        virtual void fill(const value_type &value) = 0;
+        // Removes first(old) element
+        virtual void pop() = 0;
+        // Insert element at the end(recent)
+        virtual void push(const value_type &value) = 0;
+        // Access first(old) element
+        virtual reference front(const size_type &index = 0) = 0;
+        virtual const_reference front(const size_type &index = 0) const = 0;
+        // Access last(recent) element
+        virtual reference back(const size_type &index = 0) = 0;
+        virtual const_reference back(const size_type &index = 0) const = 0;
+    };
+} // namespace ns
--- test/test.cpp
+++ test/test.cpp
@@ -1,18 +1,132 @@
 #include <catch2/catch_test_macros.hpp>
+#include <nested-shaper/sized_queue.hpp>
 
-unsigned int Factorial(unsigned int number)
+TEST_CASE("Testing sized_queue class", "[sized_queue]")
 {
-    return number <= 1 ? number : Factorial(number - 1) * number;
-}
-
-TEST_CASE("Factorials are computed", "[factorial]")
-{
-    REQUIRE(Factorial(1) == 1);
-    REQUIRE(Factorial(2) == 2);
-    REQUIRE(Factorial(3) == 6);
-    REQUIRE(Factorial(10) == 3628800);
-
-    SECTION("reserving smaller does not change size or capacity")
+    SECTION("sized_queue constructors")
     {
+        ns::sized_queue<int, 5> sized_queue_default;
+        REQUIRE(sized_queue_default.size() == 0);
+        REQUIRE(sized_queue_default.capacity() == 5);
+        REQUIRE(sized_queue_default.max_size() == sized_queue_default.capacity());
+
+        ns::sized_queue<int, 5> sized_queue_max_size{3};
+        REQUIRE(sized_queue_max_size.size() == 0);
+        REQUIRE(sized_queue_max_size.capacity() == 5);
+        REQUIRE(sized_queue_max_size.max_size() == 3);
+
+        ns::sized_queue<int, 5> sized_queue_copy{sized_queue_max_size};
+        REQUIRE(sized_queue_copy.size() == sized_queue_max_size.size());
+        REQUIRE(sized_queue_copy.capacity() == sized_queue_max_size.capacity());
+        REQUIRE(sized_queue_copy.max_size() == sized_queue_max_size.max_size());
+    }
+
+    SECTION("sized_queue methods")
+    {
+        ns::sized_queue<int, 5> sized_queue{3};
+        REQUIRE(sized_queue.size() == 0);
+        REQUIRE(sized_queue.empty() == true);
+        REQUIRE(sized_queue.full() == false);
+        sized_queue.push(17);
+        REQUIRE(sized_queue.size() == 1);
+        REQUIRE(sized_queue.empty() == false);
+        REQUIRE(sized_queue.full() == false);
+        REQUIRE(sized_queue.front() == 17);
+        REQUIRE(sized_queue.back() == 17);
+        REQUIRE(sized_queue.front(1) == 17);
+        REQUIRE(sized_queue.back(1) == 17);
+        REQUIRE(sized_queue.front(2) == 17);
+        REQUIRE(sized_queue.back(2) == 17);
+        sized_queue.push(21);
+        REQUIRE(sized_queue.size() == 2);
+        REQUIRE(sized_queue.empty() == false);
+        REQUIRE(sized_queue.full() == false);
+        REQUIRE(sized_queue.front() == 17);
+        REQUIRE(sized_queue.front(1) == 21);
+        REQUIRE(sized_queue.front(2) == 21);
+        REQUIRE(sized_queue.back() == 21);
+        REQUIRE(sized_queue.back(1) == 17);
+        REQUIRE(sized_queue.back(2) == 17);
+        sized_queue.push(27);
+        REQUIRE(sized_queue.size() == 3);
+        REQUIRE(sized_queue.empty() == false);
+        REQUIRE(sized_queue.full() == true);
+        REQUIRE(sized_queue.front() == 17);
+        REQUIRE(sized_queue.front(1) == 21);
+        REQUIRE(sized_queue.front(2) == 27);
+        REQUIRE(sized_queue.front(3) == 27);
+        REQUIRE(sized_queue.back() == 27);
+        REQUIRE(sized_queue.back(1) == 21);
+        REQUIRE(sized_queue.back(2) == 17);
+        REQUIRE(sized_queue.back(3) == 17);
+        sized_queue.push(31);
+        REQUIRE(sized_queue.size() == 3);
+        REQUIRE(sized_queue.empty() == false);
+        REQUIRE(sized_queue.full() == true);
+        REQUIRE(sized_queue.front() == 21);
+        REQUIRE(sized_queue.front(1) == 27);
+        REQUIRE(sized_queue.front(2) == 31);
+        REQUIRE(sized_queue.front(3) == 31);
+        REQUIRE(sized_queue.back() == 31);
+        REQUIRE(sized_queue.back(1) == 27);
+        REQUIRE(sized_queue.back(2) == 21);
+        REQUIRE(sized_queue.back(3) == 21);
+        sized_queue.push(34);
+        REQUIRE(sized_queue.size() == 3);
+        REQUIRE(sized_queue.empty() == false);
+        REQUIRE(sized_queue.full() == true);
+        REQUIRE(sized_queue.front() == 27);
+        REQUIRE(sized_queue.front(1) == 31);
+        REQUIRE(sized_queue.front(2) == 34);
+        REQUIRE(sized_queue.front(3) == 34);
+        REQUIRE(sized_queue.back() == 34);
+        REQUIRE(sized_queue.back(1) == 31);
+        REQUIRE(sized_queue.back(2) == 27);
+        REQUIRE(sized_queue.back(3) == 27);
+        sized_queue.pop();
+        REQUIRE(sized_queue.size() == 2);
+        REQUIRE(sized_queue.empty() == false);
+        REQUIRE(sized_queue.full() == false);
+        REQUIRE(sized_queue.front() == 31);
+        REQUIRE(sized_queue.front(1) == 34);
+        REQUIRE(sized_queue.front(2) == 34);
+        REQUIRE(sized_queue.front(3) == 34);
+        REQUIRE(sized_queue.back() == 34);
+        REQUIRE(sized_queue.back(1) == 31);
+        REQUIRE(sized_queue.back(2) == 31);
+        REQUIRE(sized_queue.back(3) == 31);
+        sized_queue.pop();
+        REQUIRE(sized_queue.size() == 1);
+        REQUIRE(sized_queue.empty() == false);
+        REQUIRE(sized_queue.full() == false);
+        REQUIRE(sized_queue.front() == 34);
+        REQUIRE(sized_queue.front(1) == 34);
+        REQUIRE(sized_queue.back() == 34);
+        REQUIRE(sized_queue.back(1) == 34);
+        sized_queue.pop();
+        REQUIRE(sized_queue.size() == 0);
+        REQUIRE(sized_queue.empty() == true);
+        REQUIRE(sized_queue.full() == false);
+        sized_queue.fill(5);
+        REQUIRE(sized_queue.size() == 3);
+        REQUIRE(sized_queue.empty() == false);
+        REQUIRE(sized_queue.full() == true);
+        REQUIRE(sized_queue.front() == 5);
+        REQUIRE(sized_queue.front(1) == 5);
+        REQUIRE(sized_queue.front(2) == 5);
+        REQUIRE(sized_queue.front(3) == 5);
+        REQUIRE(sized_queue.back() == 5);
+        REQUIRE(sized_queue.back(1) == 5);
+        REQUIRE(sized_queue.back(2) == 5);
+        REQUIRE(sized_queue.back(3) == 5);
+        sized_queue.reset();
+        REQUIRE(sized_queue.size() == 0);
+        REQUIRE(sized_queue.empty() == true);
+        REQUIRE(sized_queue.full() == false);
+        sized_queue.resize(4);
+        sized_queue.fill(5);
+        REQUIRE(sized_queue.size() == 4);
+        REQUIRE(sized_queue.empty() == false);
+        REQUIRE(sized_queue.full() == true);
     }
 }
