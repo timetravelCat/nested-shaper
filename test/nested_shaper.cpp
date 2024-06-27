b0b72cfcd0469f83719d9bd1c695ee113b2ec0c0
timetravelCat <timetraveler930@gmail.com>
2024-06-27 23:48:20 +0900

    Add tests for NestedShaperAngleCumulativeArray, NestedShaperAngleRecursiveArray

--- test/nested_shaper.cpp
+++ test/nested_shaper.cpp
@@ -279,14 +279,272 @@ TEST_CASE("NestedShaper") {
     }
 
     SECTION("NestedShaperAngleCumulative") {
+        // 5 : DerivativeOrder
+        // 5, 2, 2 : Extents
+        NestedShaperAngleCumulative<float, 5, 5, 2, 2> nested_shaper(0.1f); // all values are initialized with 0.1f
+        array<float, 5> derivatives = nested_shaper.convolute(0.1f, 0.1f);  // 0.1f is pushed into the queue, dt = 0.1f
+
+        // first value of derivatives is central point
+        REQUIRE(fabs(derivatives[0] - 0.1f) < 1.0e-3f);
+        // second value of derivatives is first derivative
+        REQUIRE(fabs(derivatives[1] - 0.0f) < 1.0e-3f);
+        // third value of derivatives is second derivative
+        REQUIRE(fabs(derivatives[2] - 0.0f) < 1.0e-3f);
+        // fourth value of derivatives is third derivative
+        REQUIRE(fabs(derivatives[3] - 0.0f) < 1.0e-3f);
+        // fifth value of derivatives is fourth derivative
+        REQUIRE(fabs(derivatives[4] - 0.0f) < 1.0e-3f);
+
+        derivatives = nested_shaper.convolute(0.2f + M_PIf * 2.0f, 0.1f); // 0.2f + M_PIf * 2.0f is pushed into the queue, dt = 0.1f
+        // 5 // 0.1f    0.1f    0.1f    0.1f    0.2f
+        // 2 //              0.1f    0.12f
+        // 2 //              0.1f    0.11f
+        //      0.1f    0.1f    0.1f   0.1f  0.105f (Expected Last Queue Data)
+
+        REQUIRE(fabs(derivatives[0] - 0.1f) < 1.0e-3f); // Central Point
+
+        constexpr auto coefficient = CENTRAL_FINITE_DIFFERENCE_COEFFICIENTS<float, 5>::value;
+        float dt{0.1f};
+        for(size_t i = 0U; i < 4U; i++) {
+            float derivative{0.0f};
+
+            derivative += coefficient[i][0] * 0.1f;
+            derivative += coefficient[i][1] * 0.1f;
+            derivative += coefficient[i][2] * 0.1f;
+            derivative += coefficient[i][3] * 0.1f;
+            derivative += coefficient[i][4] * 0.105f;
+            derivative /= dt;
+            dt *= 0.1f;
+
+            REQUIRE_THAT(derivatives[i + 1], Catch::Matchers::WithinAbs(derivative, 5.0e-2f));
+        }
     }
 
     SECTION("NestedShaperAngleRecursive") {
+        // 5 : DerivativeOrder
+        // 5, 2, 2 : Extents
+        NestedShaperAngleRecursive<float, 5, 5, 2, 2> nested_shaper(0.1f); // all values are initialized with 0.1f
+        array<float, 5> derivatives = nested_shaper.convolute(0.1f, 0.1f); // 0.1f is pushed into the queue, dt = 0.1f
+
+        // first value of derivatives is central point
+        REQUIRE(fabs(derivatives[0] - 0.1f) < 1.0e-3f);
+        // second value of derivatives is first derivative
+        REQUIRE(fabs(derivatives[1] - 0.0f) < 1.0e-3f);
+        // third value of derivatives is second derivative
+        REQUIRE(fabs(derivatives[2] - 0.0f) < 1.0e-3f);
+        // fourth value of derivatives is third derivative
+        REQUIRE(fabs(derivatives[3] - 0.0f) < 1.0e-3f);
+        // fifth value of derivatives is fourth derivative
+        REQUIRE(fabs(derivatives[4] - 0.0f) < 1.0e-3f);
+
+        derivatives = nested_shaper.convolute(0.2f + M_PIf * 2.0f, 0.1f); // 0.2f + M_PIf * 2.0f is pushed into the queue, dt = 0.1f
+        // 5 // 0.1f    0.1f    0.1f    0.1f    0.2f
+        // 2 //              0.1f    0.12f
+        // 2 //              0.1f    0.11f
+        //      0.1f    0.1f    0.1f   0.1f  0.105f (Expected Last Queue Data)
+
+        REQUIRE(fabs(derivatives[0] - 0.1f) < 1.0e-3f); // Central Point
+
+        constexpr auto coefficient = CENTRAL_FINITE_DIFFERENCE_COEFFICIENTS<float, 5>::value;
+        float dt{0.1f};
+        for(size_t i = 0U; i < 4U; i++) {
+            float derivative{0.0f};
+
+            derivative += coefficient[i][0] * 0.1f;
+            derivative += coefficient[i][1] * 0.1f;
+            derivative += coefficient[i][2] * 0.1f;
+            derivative += coefficient[i][3] * 0.1f;
+            derivative += coefficient[i][4] * 0.105f;
+            derivative /= dt;
+            dt *= 0.1f;
+
+            REQUIRE_THAT(derivatives[i + 1], Catch::Matchers::WithinAbs(derivative, 5.0e-2f));
+        }
     }
 
     SECTION("NestedShaperAngleCumulativeArray") {
+        // 5 : DerivativeOrder
+        // 5, 2, 2 : Extents
+        using array_type = array<float, 3>;
+        using derivative_type = array<array<float, 5>, 3>;
+        NestedShaperAngleCumulativeArray<float, 3, 5, 5, 2, 2> nested_shaper(array_type{0.1f, 0.2f, 0.3f}); // all values are initialized with {10.0f, 20.0f, 30.0f}
+
+        derivative_type derivatives = nested_shaper.convolute(array_type{0.1f, 0.2f, 0.3f}, 0.1f); // {10.0f, 20.0f, 30.0f} is pushed into the queue, dt = 0.1f
+        // first value of derivatives is central point
+        REQUIRE(fabs(derivatives[0][0] - 0.1f) < 1.0e-3f);
+        REQUIRE(fabs(derivatives[1][0] - 0.2f) < 1.0e-3f);
+        REQUIRE(fabs(derivatives[2][0] - 0.3f) < 1.0e-3f);
+
+        // second value of derivatives is first derivative
+        REQUIRE(fabs(derivatives[0][1] - 0.0f) < 1.0e-3f);
+        REQUIRE(fabs(derivatives[1][1] - 0.0f) < 1.0e-3f);
+        REQUIRE(fabs(derivatives[2][1] - 0.0f) < 1.0e-3f);
+
+        // third value of derivatives is second derivative
+        REQUIRE(fabs(derivatives[0][2] - 0.0f) < 1.0e-3f);
+        REQUIRE(fabs(derivatives[1][2] - 0.0f) < 1.0e-3f);
+        REQUIRE(fabs(derivatives[2][2] - 0.0f) < 1.0e-3f);
+
+        // fourth value of derivatives is third derivative
+        REQUIRE(fabs(derivatives[0][3] - 0.0f) < 1.0e-3f);
+        REQUIRE(fabs(derivatives[1][3] - 0.0f) < 1.0e-3f);
+        REQUIRE(fabs(derivatives[2][3] - 0.0f) < 1.0e-3f);
+
+        // fifth value of derivatives is fourth derivative
+        REQUIRE(fabs(derivatives[0][4] - 0.0f) < 1.0e-3f);
+        REQUIRE(fabs(derivatives[1][4] - 0.0f) < 1.0e-3f);
+        REQUIRE(fabs(derivatives[2][4] - 0.0f) < 1.0e-3f);
+
+        derivatives = nested_shaper.convolute(array_type{0.2f + M_PIf * 2.0f, 0.3f + M_PIf * 2.0f, 0.4f + M_PIf * 2.0f}, 0.1f); // {0.2f, 0.3f, 0.4f} is pushed into the queue, dt = 0.1f
+        //                     first axis
+        // 5 // 0.10f    0.10f    0.10f    0.10f    0.20f
+        // 2 //              0.10f    0.12f
+        // 2 //              0.10f    0.11f
+        //      0.10f    0.10f    0.10f   0.10f  0.105f (Expected Last Queue Data)
+
+        //                     second axis
+        // 5 // 0.20f    0.20f    0.20f    0.20f    0.30f
+        // 2 //              0.20f    0.22f
+        // 2 //              0.20f    0.21f
+        //      0.20f    0.20f    0.20f   0.20f  0.205f (Expected Last Queue Data)
+
+        //                     third axis
+        // 5 // 0.30f    0.30f    0.30f    0.30f    0.40f
+        // 2 //              0.30f    0.32f
+        // 2 //              0.30f    0.31f
+        //      0.30f    0.30f    0.30f   0.30f  0.305f (Expected Last Queue Data)
+
+        REQUIRE(fabs(derivatives[0][0] - 0.1f) < 1.0e-3f); // Central Point
+        REQUIRE(fabs(derivatives[1][0] - 0.2f) < 1.0e-3f); // Central Point
+        REQUIRE(fabs(derivatives[2][0] - 0.3f) < 1.0e-3f); // Central Point
+
+        constexpr auto coefficient = CENTRAL_FINITE_DIFFERENCE_COEFFICIENTS<float, 5>::value;
+
+        float dt{0.1f};
+        for(size_t i = 0U; i < 4U; i++) {
+            float derivative_first{0.0f};
+            float derivative_second{0.0f};
+            float derivative_third{0.0f};
+
+            derivative_first += coefficient[i][0] * 0.1f;
+            derivative_first += coefficient[i][1] * 0.1f;
+            derivative_first += coefficient[i][2] * 0.1f;
+            derivative_first += coefficient[i][3] * 0.1f;
+            derivative_first += coefficient[i][4] * 0.105f;
+            derivative_first /= dt;
+
+            derivative_second += coefficient[i][0] * 0.200f;
+            derivative_second += coefficient[i][1] * 0.200f;
+            derivative_second += coefficient[i][2] * 0.200f;
+            derivative_second += coefficient[i][3] * 0.200f;
+            derivative_second += coefficient[i][4] * 0.205f;
+            derivative_second /= dt;
+
+            derivative_third += coefficient[i][0] * 0.300f;
+            derivative_third += coefficient[i][1] * 0.300f;
+            derivative_third += coefficient[i][2] * 0.300f;
+            derivative_third += coefficient[i][3] * 0.300f;
+            derivative_third += coefficient[i][4] * 0.305f;
+            derivative_third /= dt;
+
+            dt *= 0.1f;
+
+            REQUIRE_THAT(derivatives[0][i + 1], Catch::Matchers::WithinAbs(derivative_first, 5.0e-2f));
+            REQUIRE_THAT(derivatives[1][i + 1], Catch::Matchers::WithinAbs(derivative_second, 5.0e-2f));
+            REQUIRE_THAT(derivatives[2][i + 1], Catch::Matchers::WithinAbs(derivative_third, 5.0e-2f));
+        }
     }
 
     SECTION("NestedShaperAngleRecursiveArray") {
+        // 5 : DerivativeOrder
+        // 5, 2, 2 : Extents
+        using array_type = array<float, 3>;
+        using derivative_type = array<array<float, 5>, 3>;
+        NestedShaperAngleRecursiveArray<float, 3, 5, 5, 2, 2> nested_shaper(array_type{0.1f, 0.2f, 0.3f}); // all values are initialized with {10.0f, 20.0f, 30.0f}
+
+        derivative_type derivatives = nested_shaper.convolute(array_type{0.1f, 0.2f, 0.3f}, 0.1f); // {10.0f, 20.0f, 30.0f} is pushed into the queue, dt = 0.1f
+        // first value of derivatives is central point
+        REQUIRE(fabs(derivatives[0][0] - 0.1f) < 1.0e-3f);
+        REQUIRE(fabs(derivatives[1][0] - 0.2f) < 1.0e-3f);
+        REQUIRE(fabs(derivatives[2][0] - 0.3f) < 1.0e-3f);
+
+        // second value of derivatives is first derivative
+        REQUIRE(fabs(derivatives[0][1] - 0.0f) < 1.0e-3f);
+        REQUIRE(fabs(derivatives[1][1] - 0.0f) < 1.0e-3f);
+        REQUIRE(fabs(derivatives[2][1] - 0.0f) < 1.0e-3f);
+
+        // third value of derivatives is second derivative
+        REQUIRE(fabs(derivatives[0][2] - 0.0f) < 1.0e-3f);
+        REQUIRE(fabs(derivatives[1][2] - 0.0f) < 1.0e-3f);
+        REQUIRE(fabs(derivatives[2][2] - 0.0f) < 1.0e-3f);
+
+        // fourth value of derivatives is third derivative
+        REQUIRE(fabs(derivatives[0][3] - 0.0f) < 1.0e-3f);
+        REQUIRE(fabs(derivatives[1][3] - 0.0f) < 1.0e-3f);
+        REQUIRE(fabs(derivatives[2][3] - 0.0f) < 1.0e-3f);
+
+        // fifth value of derivatives is fourth derivative
+        REQUIRE(fabs(derivatives[0][4] - 0.0f) < 1.0e-3f);
+        REQUIRE(fabs(derivatives[1][4] - 0.0f) < 1.0e-3f);
+        REQUIRE(fabs(derivatives[2][4] - 0.0f) < 1.0e-3f);
+
+        derivatives = nested_shaper.convolute(array_type{0.2f + M_PIf * 2.0f, 0.3f + M_PIf * 2.0f, 0.4f + M_PIf * 2.0f}, 0.1f); // {0.2f, 0.3f, 0.4f} is pushed into the queue, dt = 0.1f
+        //                     first axis
+        // 5 // 0.10f    0.10f    0.10f    0.10f    0.20f
+        // 2 //              0.10f    0.12f
+        // 2 //              0.10f    0.11f
+        //      0.10f    0.10f    0.10f   0.10f  0.105f (Expected Last Queue Data)
+
+        //                     second axis
+        // 5 // 0.20f    0.20f    0.20f    0.20f    0.30f
+        // 2 //              0.20f    0.22f
+        // 2 //              0.20f    0.21f
+        //      0.20f    0.20f    0.20f   0.20f  0.205f (Expected Last Queue Data)
+
+        //                     third axis
+        // 5 // 0.30f    0.30f    0.30f    0.30f    0.40f
+        // 2 //              0.30f    0.32f
+        // 2 //              0.30f    0.31f
+        //      0.30f    0.30f    0.30f   0.30f  0.305f (Expected Last Queue Data)
+
+        REQUIRE(fabs(derivatives[0][0] - 0.1f) < 1.0e-3f); // Central Point
+        REQUIRE(fabs(derivatives[1][0] - 0.2f) < 1.0e-3f); // Central Point
+        REQUIRE(fabs(derivatives[2][0] - 0.3f) < 1.0e-3f); // Central Point
+
+        constexpr auto coefficient = CENTRAL_FINITE_DIFFERENCE_COEFFICIENTS<float, 5>::value;
+
+        float dt{0.1f};
+        for(size_t i = 0U; i < 4U; i++) {
+            float derivative_first{0.0f};
+            float derivative_second{0.0f};
+            float derivative_third{0.0f};
+
+            derivative_first += coefficient[i][0] * 0.1f;
+            derivative_first += coefficient[i][1] * 0.1f;
+            derivative_first += coefficient[i][2] * 0.1f;
+            derivative_first += coefficient[i][3] * 0.1f;
+            derivative_first += coefficient[i][4] * 0.105f;
+            derivative_first /= dt;
+
+            derivative_second += coefficient[i][0] * 0.200f;
+            derivative_second += coefficient[i][1] * 0.200f;
+            derivative_second += coefficient[i][2] * 0.200f;
+            derivative_second += coefficient[i][3] * 0.200f;
+            derivative_second += coefficient[i][4] * 0.205f;
+            derivative_second /= dt;
+
+            derivative_third += coefficient[i][0] * 0.300f;
+            derivative_third += coefficient[i][1] * 0.300f;
+            derivative_third += coefficient[i][2] * 0.300f;
+            derivative_third += coefficient[i][3] * 0.300f;
+            derivative_third += coefficient[i][4] * 0.305f;
+            derivative_third /= dt;
+
+            dt *= 0.1f;
+
+            REQUIRE_THAT(derivatives[0][i + 1], Catch::Matchers::WithinAbs(derivative_first, 5.0e-2f));
+            REQUIRE_THAT(derivatives[1][i + 1], Catch::Matchers::WithinAbs(derivative_second, 5.0e-2f));
+            REQUIRE_THAT(derivatives[2][i + 1], Catch::Matchers::WithinAbs(derivative_third, 5.0e-2f));
+        }
     }
 }
